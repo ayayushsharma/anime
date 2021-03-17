583cebd588086b2d1074b0ab1f81d12aa04068cf
ayayushsharma <64801786+ayayushsharma@users.noreply.github.com>
2021-03-18 00:35:18 +0530

    Initializing

---
+++ README.md
@@ -0,0 +1,12 @@
+# Anime
+
+[![Generic badge](https://img.shields.io/badge/Being%20built%20with-React--Native-blue)](https://reactnative.dev/)
+
+Aiming for a good MyAnimeList alternative with a cleaner design and ad free experince. 
+
+# App Features
+
+- Syncronize with MyAnimeList
+- Anime Search through filters
+- Looking Up Anime songs on Streaming Platforms
+- Finding Anime Discussion Pages on reddit
---
+++ docs/assets/js/main.js
@@ -0,0 +1,19 @@
+var countDownDate = new Date("May 5, 2021 23:59:59").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("date").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("date").innerHTML = "EXPIRED";
+  }
+}, 10);
