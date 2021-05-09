dadb67f42fb0d892903e825e8c75acc4d1085eeb
ayayushsharma <64801786+ayayushsharma@users.noreply.github.com>
2021-05-10 00:39:09 +0530

    Work Starts Now

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,19 +1,20 @@
-var countDownDate = new Date("May 5, 2021 23:59:59").getTime();
+var countDownDate = new Date("June 6, 2021 23:59:59").getTime();
 
 var x = setInterval(function () {
-  var now = new Date().getTime();
+    var now = new Date().getTime();
 
-  var distance = countDownDate - now;
+    var distance = countDownDate - now;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("date").innerHTML =
-    days + "d " + hours + "h " + minutes + "m " + seconds + "s";
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("date").innerHTML = "EXPIRED";
-  }
+    document.getElementById("date").innerHTML =
+        // days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+        `${days} d ${hours} h ${minutes} m ${seconds} s`;
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("date").innerHTML = "EXPIRED";
+    }
 }, 10);
---
+++ src/screens/README.md
@@ -0,0 +1,45 @@
+# Screens
+
+This section has the source code for the Different screens that the app has. Which are namely :
+
+- [Browse](#browse)
+- [Home](#home)
+- [Stats](#stats)
+
+<h2 id="browse"><a href="Browse/">Browse 🔍</a></h2>
+
+This discover section of the app where the user can browse of new anime through :
+
+- Genre
+- Category
+- My Anime List Recommendations
+
+One Can also see the list of Top Characters
+
+- Highest Rated (most favorites)
+
+<h2 id="home"><a href="Home/">Home</a> 🏠</h2>
+
+The User's Own Anime that he has been watching. Comprises lists of :
+
+- All Anime
+- Currently Watching
+- On Hold
+- In Wishlist
+- Completed
+- Dropped
+
+<h2 id="stats"><a href="Stats/">Stats</a> 📊</h2>
+
+A Summary of How he has rated anime and what are is general watching pattern which will help him in telling what he likes to watch and choose anime with similar genres.
+
+List Comprises of :
+
+- Score Distribution Chart
+- A Genre Pentagram
+- Favorite Studios
+- Total Anime Watched
+- Total Episodes Watched
+- (Mean) Score
+
+My God this stuff will take time to complete. 😪
